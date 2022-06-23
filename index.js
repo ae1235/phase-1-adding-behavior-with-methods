8d6999250c70ac2622e1b6190fd028de57c4bbbb
+ <+>
2022-06-23 15:10:09 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 // Your code here
+class Cat {
+    
+    constructor(name, sex) {
+        this.name = name;
+        this.sex = sex;
+        this.speak = function(){
+            return `${this.name} says meow!`
+        }
+    }
+  }
+
+  class Dog {
+    
+    constructor(name, sex) {
+        this.name = name;
+        this.sex = sex;
+        this.speak = function(){
+            return `${this.name} says woof!`
+        }
+    }
+  }
+
+  class Bird {
+    
+    constructor(name, sex) {
+        this.name = name;
+        this.sex = sex;
+
+        this.speak = function() {
+            if(this.sex === "male") {
+                return `It's me! ${name}, the parrot!`
+            }
+            else {
+                return `${name} says squawk!`
+            }
+        }
+    }
+  } 
